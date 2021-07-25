d02dfbb3723d97c6f5489a13e5cec1c44826c9e3
rayhan999 <58140227+rayhan999@users.noreply.github.com>
2021-07-25 21:43:36 +0600

    Problem 2 solved

---
+++ Lec1/EvenOdd.java
@@ -0,0 +1,21 @@
+import java.util.*; 
+
+public class EvenOdd {
+    public static void main(String[] args)
+    {
+       int[] nums = {12,32,43,1,54,53,15,64,3,13};
+       int even = 0, odd = 0;
+           
+       for(int i = 0; i < nums.length; i++) {
+           if(nums[i] % 2 == 0)
+           {         
+             even++;
+           }
+           else
+              odd++;	
+       }                 
+       System.out.print(odd + " Odd Numbers ");
+       System.out.println(); 
+       System.out.print(even + " Even Numbers ");
+     }
+   }
--- Lec1/Prob1.java
+++ Lec1/Prob1.java
@@ -1,12 +1,12 @@
 public class Prob1 {  
     public static void main(String[] args) {  
-        int [] arr = new int [] {12,32,43,1,54,53,15,64,3,13};  
-        System.out.println("Input: ");  
+        int [] arr = {12,32,43,1,54,53,15,64,3,13};  
+        System.out.print("Input: ");  
         for (int i = 0; i < arr.length; i++) {  
             System.out.print(arr[i] + " ");  
         }  
         System.out.println();  
-        System.out.println("Output: ");  
+        System.out.print("Output: ");  
         for (int i = arr.length-1; i >= 0; i--) {  
             System.out.print(arr[i] + " ");  
         }  
